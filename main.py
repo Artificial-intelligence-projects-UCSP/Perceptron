af9478b08d9b330c4f1533de2830ceec6cb2dab5
capijose1 <38143262+capijose1@users.noreply.github.com>
2021-06-10 16:47:47 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,134 @@
-from keras.datasets import mnist
+import numpy as np
 import matplotlib.pyplot as plt
-# cargar (descargar si es necesario) el conjunto de datos MNIST
-(x_training, y_training), (X_evaluation, y_evaluation) = mnist.load_data()
-# plot 4 imagenes en escala de grises
-plt.subplot(221)
-plt.imshow(x_training[8], cmap=plt.get_cmap('gray'))
-plt.subplot(222)
-plt.imshow(x_training[13], cmap=plt.get_cmap('gray'))
-plt.subplot(223)
-plt.imshow(x_training[22], cmap=plt.get_cmap('gray'))
-plt.subplot(224)
-plt.imshow(x_training[5], cmap=plt.get_cmap('gray'))
-plt.show()
+import pickle
+with open("data/pickled_mnist.pkl", "br") as fh:
+    data = pickle.load(fh)
 
-print(X_evaluation.shape)
-print(x_training.shape)
-import numpy as np 
-import pandas as pd 
-import os
-import copy
-import time
-import torch
-import torch.nn as nn
-import cv2
-import matplotlib.pyplot as plt
-import copy
-import time
-import albumentations as A
-import torch_optimizer as optim
-from res_mlp_pytorch import ResMLP
-from PIL import Image
-from albumentations.pytorch import ToTensorV2
-from torch.utils.data import Dataset, DataLoader
+train_imgs = data[0]
+test_imgs = data[1]
+train_labels = data[2]
+test_labels = data[3]
+train_labels_one_hot = data[4]
+test_labels_one_hot = data[5]
+image_size = 28 # 
+numero_de_num = 10 
+image_pixels = image_size * image_size
+
+@np.vectorize
+def sigmoid(x):
+    return 1 / (1 + np.e ** -x)
+activation_function = sigmoid
+
+from scipy.stats import truncnorm
+
+def truncated_normal(mean=0, sd=1, low=0, upp=10):
+    return truncnorm((low - mean) / sd, 
+                     (upp - mean) / sd, 
+                     loc=mean, 
+                     scale=sd)
+
+
+class NeuralNetwork:
+    
+    def __init__(self, 
+                 num_nodos, 
+                 num_nodos_salida, 
+                 num_nodos_ocultos,
+                 tasa_aprendizaje):
+        self.num_nodos = num_nodos
+        self.num_nodos_salida = num_nodos_salida
+        self.num_nodos_ocultos = num_nodos_ocultos
+        self.tasa_aprendizaje = tasa_aprendizaje 
+        self.create_weight_matrices()
+        
+    def create_weight_matrices(self):
+        rad = 1 / np.sqrt(self.num_nodos)
+        X = truncated_normal(mean=0, 
+                             sd=1, 
+                             low=-rad, 
+                             upp=rad)
+        self.wih = X.rvs((self.num_nodos_ocultos,self.num_nodos))
+        rad = 1 / np.sqrt(self.num_nodos_ocultos)
+        X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
+        self.who = X.rvs((self.num_nodos_salida,self.num_nodos_ocultos))
+        
+    
+    def train(self, input_vector, target_vector):
+        input_vector = np.array(input_vector, ndmin=2).T
+        target_vector = np.array(target_vector, ndmin=2).T
+        
+        output_vector1 = np.dot(self.wih, 
+                                input_vector)
+        output_hidden = activation_function(output_vector1)
+        
+        output_vector2 = np.dot(self.who, 
+                                output_hidden)
+        output_network = activation_function(output_vector2)
+        
+        output_errors = target_vector - output_network
+        # update the weights:
+        tmp = output_errors * output_network \
+              * (1.0 - output_network)     
+        tmp = self.tasa_aprendizaje  * np.dot(tmp, 
+                                           output_hidden.T)
+        self.who += tmp
+
+
+        # calculate hidden errors:
+        hidden_errors = np.dot(self.who.T, 
+                               output_errors)
+        # update the weights:
+        tmp = hidden_errors * output_hidden * \
+              (1.0 - output_hidden)
+        self.wih += self.tasa_aprendizaje \
+                          * np.dot(tmp, input_vector.T)
+        
+
+        
+    i=0
+    def run(self, input_vector):
+        input_vector = np.array(input_vector, ndmin=2).T
+        output_vector = np.dot(self.wih,input_vector)
+        output_vector = activation_function(output_vector)
+        output_vector = np.dot(self.who,output_vector)
+        output_vector = activation_function(output_vector)
+    
+        return output_vector
+            
+    def confusion_matrix(self, data_array, labels):
+        cm = np.zeros((10, 10), int)
+        for i in range(len(data_array)):
+            res = self.run(data_array[i])
+            res_max = res.argmax()
+            target = labels[i][0]
+            cm[res_max, int(target)] += 1
+        return cm    
+
+    def precision(self, label, confusion_matrix):
+        col = confusion_matrix[:, label]
+        return confusion_matrix[label, label] / col.sum()
+    
+    def recall(self, label, confusion_matrix):
+        row = confusion_matrix[label, :]
+        return confusion_matrix[label, label] / row.sum()
+        
+    
+    def evaluate(self, data, labels):
+        corrects, wrongs = 0, 0
+        for i in range(len(data)):
+            res = self.run(data[i])
+            res_max = res.argmax()
+            if res_max == labels[i]:
+                corrects += 1
+            else:
+                wrongs += 1
+        return corrects, wrongs
+ANN = NeuralNetwork(num_nodos = image_pixels, 
+                    num_nodos_salida = 10, 
+                    num_nodos_ocultos = 100,
+                    tasa_aprendizaje = 0.1)
+for i in range(len(train_imgs)):
+    ANN.train(train_imgs[i], train_labels_one_hot[i])
+for i in range(20):
+    res = ANN.run(test_imgs[i])
+    print(test_labels[i], np.argmax(res), np.max(res)) 
